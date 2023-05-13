5e9e8661cd18ef3e141746cf9499019115b02a72
AnaBeatriz42 <beatrizsilvarn2016@gmail.com>
2023-05-13 10:17:31 -0300

    Resolução dos exercicios da semana 10 13/05/2023

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+function InformacaoPessoal(props) {
+  return (<div>
+    <h1>Nome:{props.name} </h1>
+    <h2>cpf:{props.cpf} </h2>
+    <h2>Endereco: {props.endereco}</h2>
+    <h2>Email: {props.email} </h2>  </div>)
+}
+
+function Operation(props) {
+  return (<div>
+    <h1>A soma entre {props.n1} e {props.n2} é {parseFloat(props.n1) + parseFloat(props.n2)} </h1>
+    <h1>A subtração entre {props.n1} e {props.n2} é {parseFloat(props.n1) - parseFloat(props.n2)}</h1>
+    <h1>A Multiplicação entre {props.n1} e {props.n2}  é {parseFloat(props.n1) * parseFloat(props.n2)} </h1>
+    <h1>A Divisão entre {props.n1} e {props.n2}  é {parseFloat(props.n1) / parseFloat(props.n2)} </h1>
+  </div>)
+}
+
+function App() {
+  return (
+    <div>
+      <InformacaoPessoal name="Anna" cpf="362.847.110-93" endereco="Rua maria das dores N° 45" email="ana.2023@gmail.com" />
+      <InformacaoPessoal name="Victor" cpf="891.501.210-06" endereco="Avenida das goibeiras N° 13" email="victorqqt.ss@gmail.com" />
+      <Operation n1="100" n2="50" />
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
